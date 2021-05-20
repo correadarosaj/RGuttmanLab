958ce97de41980ee082a2df9dad54c142bed8931
correadarosaj <joelrosa@uol.com.br>
2021-05-20 14:38:24 -0400

    modify function

--- R/getSubjLevelData.R
+++ R/getSubjLevelData.R
@@ -1,5 +1,5 @@
 getSubjLevelData = function(df,varnames){
   require(tidyverse)
-  sbj = df %>% dplyr::select(all_of(varnames)) %>% distinct()
+  sbj = df %>% dplyr::select(all_of(varnames)) %>% na.omit() %>% distinct()
   return(sbj)
 }
