73e96d01fcd7d8e76a8f950f17fd493e4d6cd579
correadarosaj <joelrosa@uol.com.br>
2021-05-20 16:11:04 -0400

    temporal matrix

---
+++ R/getTemporalMatrix.R
@@ -0,0 +1,22 @@
+# dx is a dataframe with 3 metadata columns : SUBJID , timepoint and tissue
+# the remaining columns are biomarkers metrics
+
+get.temporal.matrix<-function(dx = anx.d,
+                              tx = 'V3',
+                              tsx = 'NL'
+){
+
+  mdx = reshape2::melt(dx,id.vars=c('SUBJID','timepoint','tissue'))
+
+
+  # filter by timepoint
+  mat.time   = mdx %>% filter(timepoint == tx  & tissue == tsx)
+
+  # reshape to data matrix
+  mat = reshape2::dcast(mat.time %>% dplyr::select(SUBJID, biomarker = variable,
+                                                   expression = value),
+                        SUBJID~biomarker,value.var = 'expression') %>%
+    column_to_rownames(var = 'SUBJID')
+
+  return(mat)
+}
