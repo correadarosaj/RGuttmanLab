cc73adade7cc848789e10cb2625d1eb5ad1e9977
correadarosaj <joelrosa@uol.com.br>
2021-05-21 11:42:45 -0400

    a new file and a deleted one

---
+++ R/extractMetadata.R
@@ -0,0 +1,13 @@
+extractMetadata = function(df,varnames,idvar){
+
+  list_df =  lapply(varnames, function(x) df %>%
+                      dplyr::select(SUBJID = all_of(idvar), all_of(x)) %>%
+                      na.omit() %>%
+                      distinct())
+
+  metadata = list_df %>% reduce(left_join , by = 'SUBJID')
+
+  return(metadata)
+
+
+}
--- R/getSubjLevelData.R
+++
@@ -1,5 +0,0 @@
-getSubjLevelData = function(df,varnames){
-  require(tidyverse)
-  sbj = df %>% dplyr::select(all_of(varnames)) %>% na.omit() %>% distinct()
-  return(sbj)
-}
